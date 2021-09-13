e26d31f59826b9b65387c060676572fd849f90a0
ninjasujan <sujan@appyhigh.com>
2021-09-14 01:21:09 +0530

    pre post hooks added in test cases

--- src/03-setting-tearing/seting.tearing.test.ts
+++ src/03-setting-tearing/seting.tearing.test.ts
@@ -1,5 +1,38 @@
-let list = [];
+let list: string[] = [];
 
-describe("seting and trearing test", () => {
-	it("shoping list should contain apple", () => {});
+beforeAll(() => {
+	console.log("[Before All]");
+	list = ["bread", "milk", "jam", "eggs", "apple"];
 });
+
+beforeEach(() => {
+	console.log("[Before Each]");
+	list = ["bread", "milk", "jam", "eggs", "apple"];
+});
+
+afterEach(() => {
+	console.log("[After Each]");
+	list = ["bread", "milk", "jam", "eggs", "apple"];
+});
+
+afterAll(() => {
+	console.log("[After All]");
+	list = ["bread", "milk", "jam", "eggs", "apple"];
+});
+
+describe("setting and trearing test", () => {
+	it("shopping list should contain apple", () => {
+		expect(list).toContain("apple");
+	});
+
+	it("add new item to shopping list", () => {
+		list.push("fish");
+		expect(list).toContain("fish");
+	});
+
+	it("should contain only 3", () => {
+		expect(list.length).toBe(5);
+	});
+});
+
+// it.only() only runs the particular tests
